dfdf8b5502d9482802c588ce9dc46072e7d1754e
Karunesh Sachanandani <karuneshsachanandani@outlook.com>
2021-03-05 01:05:55 -0800

    Fixing exception on no voice input

--- modules/VoiceRecognition/speechRun.py
+++ modules/VoiceRecognition/speechRun.py
@@ -13,15 +13,20 @@ if len(sys.argv) == 2:
     transmitterInstance.setTopic(topic)
 voiceInstance.speechGet()
 transmitterInstance.setCommand(voiceInstance.getCommand())
-while voiceInstance.getCommand() == "ERROR":
+voiceattempt = 0
+while voiceInstance.getCommand() == "ERROR" and voiceattempt < 2:
     print("Unrecognized input. Please enter your voice command again.")
+    voiceattempt = voiceattempt + 1
     voiceInstance.speechGet()
     transmitterInstance.setCommand(voiceInstance.getCommand())
-print("Got it. We're on it now.");
-transmitterInstance.setSongname(voiceInstance.getSongname())
-transmitterInstance.setArtistname(voiceInstance.getArtistname())
-transmitterInstance.setSongtime(voiceInstance.getSongtime())
-client = transmitterInstance.connect_mqtt()
-client.loop_start()
-transmitterInstance.publish(client)
-client.loop_stop()
+if voiceattempt == 2:
+    print("Sorry, we couldn't hear anything from your mic. Please try again later.")
+else:
+    print("Got it. We're on it now.")
+    transmitterInstance.setSongname(voiceInstance.getSongname())
+    transmitterInstance.setArtistname(voiceInstance.getArtistname())
+    transmitterInstance.setSongtime(voiceInstance.getSongtime())
+    client = transmitterInstance.connect_mqtt()
+    client.loop_start()
+    transmitterInstance.publish(client)
+    client.loop_stop()
